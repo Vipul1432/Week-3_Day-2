88fa7347489f8070cb17a4b012e6c81ed96decf2
Vipul143212 <vipulu563@gmail.com>
2023-08-22 10:36:36 +0530

    Add greet program

---
+++ Exercise-1/src/app.ts
@@ -0,0 +1,12 @@
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+  }
+  
+   /* Test the greet function */
+  console.log(greet("Vipul Kumar"));         
+  console.log(greet(""));             
+  console.log(greet("123"));      
+  console.log(greet("John Doe"));  
+  console.log(greet("   Spaces   ")); 
+  console.log(greet("Special-Name")); 
+  
