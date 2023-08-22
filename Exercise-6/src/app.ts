47da34f8c84dd97ce3bad37071b4ba39f425dd02
Vipul143212 <vipulu563@gmail.com>
2023-08-22 11:40:46 +0530

    Add bug and find using vs code debugger tools

---
+++ Exercise-6/src/app.ts
@@ -0,0 +1,13 @@
+function calculateAverage(numbers: number[]): number {
+  let sum = 0;
+  for (let num of numbers) {
+    sum += num;
+  }
+  // Add bug: Should be sum / numbers.length, not numbers.length / sum
+  return  numbers.length / sum; 
+}
+
+const numbers = [10, 20, 30, 40, 50];
+
+const average = calculateAverage(numbers);
+console.log(`Average: ${average}`);
